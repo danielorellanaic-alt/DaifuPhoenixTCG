6feb139845f9466ed80edaf50c92d6bc68182d14
Daniel Orellana <daniel.orellana.ic@gmail.com>
2026-05-28 23:58:13 -0400

    Update visuals

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,47 +1,10 @@
-import {
-  Flame,
-  MapPin,
-  ShoppingBag,
-} from "lucide-react";
-
-import storeConfig from "../data/storeConfig";
-import storeLogo from "../assets/logo/store-logo.png";
 import storeBanner from "../assets/logo/store-banner.png";
 
 export default function Hero() {
   return (
     <section className="hero-store" id="inicio">
       <div className="banner">
-        <img src={storeBanner} alt="banner tienda" />
-      </div>
-
-      <div className="store-profile">
-        <img
-          className="store-logo"
-          src={storeLogo}
-          alt={storeConfig.store.name}
-        />
-
-        <div className="store-info">
-          <h2>{storeConfig.store.name}</h2>
-
-          <div className="store-meta">
-            <p>
-              <Flame size={16} />
-              {storeConfig.home.metaLines[0]}
-            </p>
-
-            <p>
-              <MapPin size={16} />
-              {storeConfig.home.metaLines[1]}
-            </p>
-
-            <p>
-              <ShoppingBag size={16} />
-              {storeConfig.home.metaLines[2]}
-            </p>
-          </div>
-        </div>
+        <img src={storeBanner} alt="Banner tienda" />
       </div>
     </section>
   );
