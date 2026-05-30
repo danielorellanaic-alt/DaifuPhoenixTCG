8d2cb332c4767f52ce5b0638318773cfd3bd9e22
Daniel Orellana <daniel.orellana.ic@gmail.com>
2026-05-30 01:33:44 -0400

    Update visuals

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,22 @@ export default function Hero() {
   return (
     <section className="hero-store" id="inicio">
       <div className="banner">
-        <img src={storeBanner} alt="Banner tienda" />
+        <img
+          src={storeBanner}
+          alt="Banner tienda"
+        />
+      </div>
+
+      <div className="store-marquee">
+        <div className="store-marquee-track">
+          <span>Pokémon TCG Competitivo & Coleccionismo</span>
+          <span>Tienda online • Región de Valparaíso</span>
+          <span>Singles • Sellado • Accesorios</span>
+
+          <span>Pokémon TCG Competitivo & Coleccionismo</span>
+          <span>Tienda online • Región de Valparaíso</span>
+          <span>Singles • Sellado • Accesorios</span>
+        </div>
       </div>
     </section>
   );
