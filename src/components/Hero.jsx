50317055818dbe1c9cdc891741e7837257f78983
Daniel Orellana <daniel.orellana.ic@gmail.com>
2026-05-30 01:39:33 -0400

    Update visuals

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,24 +1,25 @@
 import storeBanner from "../assets/logo/store-banner.png";
 
+const marqueeItems = [
+  "Pokémon TCG Competitivo & Coleccionismo",
+  "Tienda online • Región de Valparaíso",
+  "Singles • Sellado • Accesorios",
+];
+
 export default function Hero() {
   return (
     <section className="hero-store" id="inicio">
       <div className="banner">
-        <img
-          src={storeBanner}
-          alt="Banner tienda"
-        />
+        <img src={storeBanner} alt="Banner tienda" />
       </div>
 
       <div className="store-marquee">
         <div className="store-marquee-track">
-          <span>Pokémon TCG Competitivo & Coleccionismo</span>
-          <span>Tienda online • Región de Valparaíso</span>
-          <span>Singles • Sellado • Accesorios</span>
-
-          <span>Pokémon TCG Competitivo & Coleccionismo</span>
-          <span>Tienda online • Región de Valparaíso</span>
-          <span>Singles • Sellado • Accesorios</span>
+          {[...marqueeItems, ...marqueeItems, ...marqueeItems].map(
+            (item, index) => (
+              <span key={`${item}-${index}`}>{item}</span>
+            )
+          )}
         </div>
       </div>
     </section>
